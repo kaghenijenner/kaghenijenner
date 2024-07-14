8fc979eff157da6e0bdfe28eba37c9d05330f269
jennermaxim <jennersi1remaxim@gmail.com>
2024-07-14 14:51:45 +0300

    Start My Personal Website

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+const App = () => {
+
+  return (
+    <>
+      <h1>Under Contruction</h1>
+      <a href="https://github.com/jennermaxim">Jenner Maxim</a>
+    </>
+  )
+}
+
+export default App
