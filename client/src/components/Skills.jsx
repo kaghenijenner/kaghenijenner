89266ebda0ec6f28087a1b3a75e7602c6a3ee491
jennermaxim <jennersi1remaxim@gmail.com>
2024-12-19 19:50:40 +0300

    Add new skills

--- client/src/components/Skills.jsx
+++ client/src/components/Skills.jsx
@@ -12,21 +12,33 @@ import Flutter from "../assets/flutter.png";
 import Python from "../assets/python.jpg";
 import Linux from "../assets/linux.png";
 import Node from "../assets/nodejs.png";
+import JQuery from "../assets/jquery.png";
+import AJAX from "../assets/ajax.jpeg";
+import Angular from "../assets/angular.png";
+import Nextjs from "../assets/nextjs.jpg";
+import TypeScript from "../assets/typescript.png";
+import WordPress from "../assets/wordpress.png";
 
 const Skills = () => {
     const skills = [
         { img: HTMLCSS, title: "HTML & CSS", level: 100 },
-        { img: JS, title: "JavaScript", level: 80 },
-        { img: ReactJS, title: "React", level: 70 },
+        { img: JS, title: "JavaScript", level: 90 },
+        { img: ReactJS, title: "React.js", level: 70 },
         { img: PHP, title: "PHP", level: 100 },
         { img: MySQL, title: "MySQL", level: 100 },
         { img: Firebase, title: "Firebase", level: 50 },
-        { img: Python, title: "Python", level: 20 },
+        { img: Python, title: "Python", level: 50 },
         { img: PhotoShop, title: "PhotoShop", level: 90 },
         { img: Blender, title: "Blender", level: 80 },
-        { img: Flutter, title: "Flutter", level: 50 },
+        { img: Flutter, title: "Flutter", level: 60 },
         { img: Linux, title: "Kali Linux", level: 100 },
-        { img: Node, title: "Nodejs", level: 20 }
+        { img: Node, title: "Node.js", level: 20 },
+        { img: JQuery, title: "JQuery", level: 100 },
+        { img: AJAX, title: "Ajax", level: 100 },
+        { img: Angular, title: "Angular.js", level: 50 },
+        { img: Nextjs, title: "Next.js", level: 50 },
+        { img: TypeScript, title: "TypeScript", level: 50 },
+        { img: WordPress, title: "WordPress", level: 100 },
     ];
     const sortedSkills = skills.sort((a, b) => b.level - a.level);
     return (
